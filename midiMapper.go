a51257b9831f10cb1ae8ff9b8452f7ffed84d46b
GeorgeLuo <georgeluo@Georges-Mini.fios-router.home>
2018-08-06 02:05:51 -0400

    add option to specify channel through software

--- midiMapper.go
+++ midiMapper.go
@@ -21,11 +21,57 @@ type DeviceLayout struct {
 	devices []Device
 }
 
+const (
+
+/*
+	declare constants for channel 1-3
+	10010000= 90= 144| Chan 1      Note on        |      "        |      "
+	10010001= 91= 145| Chan 2         "           |      "        |      "
+	10010010= 92= 146| Chan 3         "           |      "        |      "
+
+	10000000= 80= 128| Chan 1      Note off       |  Note Number  | Note Velocity
+	10000001= 81= 129| Chan 2         "           |   (0-127)     |   (0-127)
+	10000010= 82= 130| Chan 3         "           |     see       |      "
+*/
+
+	channel1On = 144
+	channel2On = 145
+	channel3On = 146
+
+	channel1Off = 128
+	channel2Off = 129
+	channel3Off = 130
+)
+
 type Device struct {
 	name string
 	input bool
 	output bool
 	open bool
+}
+
+var OnChannels = [3]int64{channel1On, channel2On, channel3On}
+var OffChannels = [3]int64{channel1Off, channel2Off, channel3Off}
+
+// TODO: load in another class
+
+var OnToOffSet = make(map[int]int)
+var OnSet = make(map[int]bool)
+var OffSet = make(map[int]bool)
+
+func initializeChannelConstants() {
+
+	OnToOffSet[channel1On] = 128
+	OnToOffSet[channel2On] = 129
+	OnToOffSet[channel2On] = 130
+
+	OnSet[channel1On] = true
+	OnSet[channel2On] = true
+	OnSet[channel3On] = true
+
+	OffSet[channel1Off] = true
+	OffSet[channel2Off] = true
+	OffSet[channel3Off] = true
 }
 
 func initializeDeviceLayout() {
@@ -83,17 +129,50 @@ func readFromIncomingDevices(streams []portmidi.Stream) {
 	}
 }
 
-func readFromDeviceWriteToDevice(OutStream portmidi.Stream, InStream portmidi.Stream, maxEvents int) {
+func isOnCommand(command int64) bool {
+	if OnSet[int(command)] {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isOffCommand(command int64) bool {
+	if OffSet[int(command)] {
+		return true
+	} else {
+		return false
+	}
+}
+
+func onCommand(channel int) int64 {
+	return OnChannels[channel]
+}
+
+func offCommand(channel int) int64 {
+	return OffChannels[channel]
+}
+
+func readFromDeviceWriteToDevice(OutStream portmidi.Stream, InStream portmidi.Stream, maxEvents int, outChannel int) {
+	var status int64
+
 	for {
 		events, err := OutStream.Read(maxEvents)
 		if err != nil {
 			log.Fatal(err)
 		} else {
+			var sendEvents = make([]portmidi.Event, len(events))
 			if len(events) > 0 {
-				InStream.Write(events)
 				for j := 0; j < len(events); j++ {
 					fmt.Println(events[j].Timestamp, events[j].Status, events[j].Data1, events[j].Data2)
+					if isOnCommand(events[j].Status) {
+						status = onCommand(outChannel)
+					} else if isOffCommand(events[j].Status) {
+						status = offCommand(outChannel)
+					}
+					sendEvents[j] = portmidi.Event{Timestamp: events[j].Timestamp, Status: status, Data1: events[j].Data1, Data2: events[j].Data2}
 				}
+				InStream.Write(sendEvents)
 			}
 		}
 	}
@@ -103,6 +182,7 @@ func main(){
 	fmt.Printf("Reading from midi channels\n")
 	
 	portmidi.Initialize()
+	initializeChannelConstants()
 
 	var numDevices = portmidi.CountDevices() // returns the number of MIDI devices
 	fmt.Printf("Num devices: %d\n\n", numDevices)
@@ -127,6 +207,6 @@ func main(){
 		log.Fatal(err)
 	}
 
-	readFromDeviceWriteToDevice(*InStream, *OutStream, 10)
+	readFromDeviceWriteToDevice(*InStream, *OutStream, 10, 2)
 
 }
